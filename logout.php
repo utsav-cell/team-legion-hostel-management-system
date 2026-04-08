44d26463cd770d762a306cf551318088db7eb0d9
Aditya9808 <adityashiwakoti54@gmail.com>
2026-04-08 16:04:27 +0545

    feature updated

--- logout.php
+++ logout.php
@@ -5,9 +5,14 @@
 
 require_once 'dp.php';
 
+$auth = get_auth();
+if ($auth) {
+    revoke_user_tokens((int)$auth['id']);
+}
+
 // Clear JWT cookie
 if (isset($_COOKIE['auth_token'])) {
-    setcookie('auth_token', '', time() - 3600, '/');
+    setcookie('auth_token', '', auth_cookie_options(time() - 3600));
 }
 
 // Clear session
